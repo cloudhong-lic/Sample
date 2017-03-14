7460f12804ec06b4802c38fd7ac8638aedf9d6c2
Cloud Hong <hongweiyun@gmail.com>
2017-03-14 21:46:18 +1300

    develop - 添加注释

--- Sample.Datastore/Update_local_database.bat
+++ Sample.Datastore/Update_local_database.bat
@@ -1,5 +1,7 @@
-﻿:: Make sure the file using Encode in ANSI, then to prevent the weird characters (´╗┐) at the start of a batch file
-:: This file must run at the root path of this project
+﻿:: 要确保文件被设置为 Encode in ANSI, 这样在运行时就不会在命令行开头出现奇怪的字符 (´╗┐) 了
+:: 使用RoundHousE nuget package, 这样就可以不必将rh.exe纳入项目中
+:: TODO: 此批文件必须放在项目的根目录中, 否则运行有问题, 原因不明(可能是因为最后一个--files的配置问题)
+:: rh.exe的使用参见 https://github.com/chucknorris/roundhouse/wiki/ConfigurationOptions
 
 ..\packages\roundhouse.0.8.6\bin\rh.exe --sc RoundhousE --cs "Data Source=.\SQLEXPRESS;Initial Catalog=Sample_local;Integrated Security=true" --files RoundhousE
 pause
