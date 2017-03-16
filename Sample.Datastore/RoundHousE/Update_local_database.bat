139297b2d7bb2045afba036b0e8a77d8003cb926
Cloud Hong <Weiyun.Hong@lic.co.nz>
2017-03-16 16:04:33 +1300

    develop - 更新RoundHousE脚本, 解决SCHEMA问题

--- Sample.Datastore/RoundHousE/Update_local_database.bat
+++ Sample.Datastore/RoundHousE/Update_local_database.bat
@@ -1,7 +1,11 @@
 ﻿:: 要确保文件被设置为 Encode in ANSI, 这样在运行时就不会在命令行开头出现奇怪的字符 (´╗┐) 了
 :: 使用RoundHousE nuget package, 这样就可以不必将rh.exe纳入项目中
-:: 注意rh.exe所在的目录路径, 以及最后一个--files参数的路径
 :: rh.exe的使用参见 https://github.com/chucknorris/roundhouse/wiki/ConfigurationOptions
 
-..\..\packages\roundhouse.0.8.6\bin\rh.exe --sc RoundhousE --cs "Data Source=.\SQLEXPRESS;Initial Catalog=Sample_local;Integrated Security=true" --files ..\RoundhousE
+:: TODO: 注意rh.exe所在的目录路径, 以及最后一个--files参数的路径. 可以省略??
+
+:: --sc后面是RoundHousE在数据库中自动生成的table的前置名词: 例如Sample.ScriptsRun
+:: 只要--sc后面的前置名词设置地与SCHEMA一致, 就不需要添加CREATE SCHEMA [Sample] AUTHORIZATION [dbo], 因为RoundHousE在创建三个内置脚本表的时候会创建的
+
+..\..\packages\roundhouse.0.8.6\bin\rh.exe --sc "Sample" --cs "Data Source=.\SQLEXPRESS;Initial Catalog=Sample_local;Integrated Security=true" --files ..\RoundhousE
 pause
--- Sample.Datastore/RoundHousE/up/0001 - Create Schema Sample.sql
+++
@@ -1,6 +0,0 @@
-﻿-- 添加SCHEMA
--- 只有添加了SCHEMA后, 才能操作表集合, 例如[Sample].[Sex]
--- TODO: CREATE SCHEMA不能使用BEGIN TRAN和COMMIT, 不知道什么原因
-
-CREATE SCHEMA [Sample] AUTHORIZATION [dbo]
-GO
