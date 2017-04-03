64fe7e5840d798b9735a9e941de2f1fc51a360f2
Cloud Hong <Weiyun.Hong@lic.co.nz>
2017-04-04 10:57:00 +1200

    develop - 增加webpack和npm配置文件, 并简单实现React功能

---
+++ Sample.WebSite.React/webpack.config.js
@@ -0,0 +1,23 @@
+﻿var webpack = require('webpack');
+var path = require('path');
+
+var APP_DIR = path.resolve(__dirname, 'src/scripts');
+
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        filename: "./dist/scripts/bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: "babel-loader",
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+};
+
+module.exports = config;
