19b5c0a044c89fd30afc85e13c3a9442662c35b3
Cloud Hong <hongweiyun@gmail.com>
2017-04-04 20:57:17 +1200

    develop - 添加一些注释

---
+++ Sample.WebSite.React/Readme.markdown
@@ -0,0 +1,7 @@
+# Sample.WebSite.React
+
+此项目使用MVC5搭载React+Redux前端, 配置采用npm+webpack
+
+MVC使用最简单配置, 直接用VS自带的模板. Uninstall了和前端有关的packages, 比如JQUERY等等
+
+只保留了一个controller, 一个view, 没有model. TODO: 以后可能会根据轮子需要增加
--- Sample.WebSite.React/webpack.config.js
+++ Sample.WebSite.React/webpack.config.js
@@ -1,21 +1,26 @@
 ﻿var webpack = require('webpack');
 var path = require('path');
 
-var APP_DIR = path.resolve(__dirname, 'src/scripts');
+// base路径
+// __dirname为当前文件所在路径
+var ENTRY_DIR = path.resolve(__dirname, 'src/scripts');
+var OUTPUT_DIR = path.resolve(__dirname, 'dist/scripts');
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+    // 入口文件路径
+    entry: ENTRY_DIR + '/index.jsx',
+
+    // 打包输出文件名和路径, 可以不包括在github中
     output: {
-        filename: "./dist/scripts/bundle.js"
+        // path: __dirname, 
+
+        // filename: "./dist/scripts/bundle.js"
+        filename: OUTPUT_DIR + '/bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: "babel-loader",
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
+        loaders: [
+            // .jsx 文件使用babel-loader来编译处理
+            { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/, query: { presets: ['es2015', 'react'] }
         }]
     }
 };
